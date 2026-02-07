e25b75de0e7498492fc2b9f2df310610ce880e2e
lammeth <lammeth@gmail.com>
2026-02-06 21:59:26 -0600

    Update general.js
    Author

--- router/general.js
+++ router/general.js
@@ -28,11 +28,27 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
 
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const result = {};
+
+  // Get all keys (ISBNs) from books object
+  const bookKeys = Object.keys(books);
+
+  // Iterate through books and match author
+  bookKeys.forEach((key) => {
+    if (books[key].author.toLowerCase() === author.toLowerCase()) {
+      result[key] = books[key];
+    }
+  });
+
+  if (Object.keys(result).length > 0) {
+    return res.status(200).json(result);
+  } else {
+    return res.status(404).json({ message: "No books found for this author" });
+  }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
