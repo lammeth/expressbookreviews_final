0c75f8fcc34bbdaadf7c9f26b36958b544f9d30c
lammeth <lammeth@gmail.com>
2026-02-06 22:57:14 -0600

    Update general.js

--- router/general.js
+++ router/general.js
@@ -1,10 +1,11 @@
 const express = require('express');
 let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
+//let isValid = require("./auth_users.js").isValid;
+//let users = require("./auth_users.js").users;
 const public_users = express.Router();
 let { users, isValid } = require("./auth_users.js"); 
 
+// Register a new user
 public_users.post("/register", (req, res) => {
   const { username, password } = req.body;
 
